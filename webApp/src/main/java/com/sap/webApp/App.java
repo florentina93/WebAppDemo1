0cb3ebc3ecca034a129b61762f0ce771b0003c95
Patricia008 <petrut.patricia@gmail.com>
2016-07-07 10:47:30 +0300

    Revert "functional search button"
    This reverts commit 38a13c94733883d247d4efe916af215731643ce4.

--- webApp/src/main/java/com/sap/webApp/App.java
+++ webApp/src/main/java/com/sap/webApp/App.java
@@ -1,26 +1,13 @@
 package com.sap.webApp;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 /**
  * Hello world!
  *
  */
-
-@WebServlet("/wApp")
-public class App extends HttpServlet
+public class App 
 {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	// TODO Auto-generated method stub
-    	PrintWriter out = resp.getWriter();
-    	out.println("Hello from java");
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
