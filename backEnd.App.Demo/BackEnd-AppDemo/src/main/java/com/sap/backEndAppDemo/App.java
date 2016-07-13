3f4fa996bf2f199b02023413a0dc8ce7e545b531
florentina93 <and.dragu@gmail.com>
2016-07-13 11:17:15 +0300

    backend-appdemo created war

---
+++ backEnd.App.Demo/BackEnd-AppDemo/src/main/java/com/sap/backEndAppDemo/App.java
@@ -0,0 +1,13 @@
+package com.sap.backEndAppDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
